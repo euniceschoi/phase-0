84b492c0bfc01f9fa47ac2041622403fea6caec2
euniceschoi <euniceschoi93@gmail.com>
2015-09-12 01:55:38 -0400

    add longest integer progress

---
+++ week-4/longest-string/my_solution.rb
@@ -0,0 +1,31 @@
+# Longest String
+
+# I worked on this challenge [by myself, with: ].
+
+# longest_string is a method that takes an array of strings as its input
+# and returns the longest string
+#
+# +list_of_words+ is an array of strings
+# longest_string(list_of_words) should return the longest string in +list_of_words+
+#
+# If +list_of_words+ is empty the method should return nil
+
+
+# Your Solution Below
+def longest_string(list_of_words)
+  sort = 0
+  index = 0
+  if list_of_words.count  == 0
+    return nil
+  elsif
+      list_of_words.each_with_index do |string, ind|
+      if string.length > sort
+        sort == string.length
+      end
+  index == ind
+  puts list_of_words[index]
+  end
+end
+end
+
+longest_string(['cat', 'aaaaaaa', 'apples'])
