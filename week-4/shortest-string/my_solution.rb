41185c03245083e3c7cd1dec48c704cc4d233916
euniceschoi <euniceschoi93@gmail.com>
2015-09-12 01:41:02 -0400

    finalize answer for shortest string challenge

--- week-4/shortest-string/my_solution.rb
+++ week-4/shortest-string/my_solution.rb
@@ -1,6 +1,6 @@
 # Shortest String
 
-# I worked on this challenge [by myself, with: ].
+# I worked on this challenge [by myself].
 
 # shortest_string is a method that takes an array of strings as its input
 # and returns the shortest string
@@ -11,3 +11,50 @@
 # If +list_of_words+ is empty the method should return nil
 
 #Your Solution Below
+
+
+# def shortest_string(list_of_words)
+#   length_of_words=[]
+#   index=0
+#   if list_of_words.count > 1
+#     list_of_words.each_with_index do |string, ind|
+#     length_of_words << string.length
+#     length_of_words.sort
+#     list_of_words.eac
+#     index=ind
+#     end
+#     return list_of_words[index.to_i]
+#   else
+#     return list_of_words[0]
+#   end
+# end
+
+# def shortest_string(list_of_words)
+#   while list_of_words.count > 0
+#     list_of_words.each do |string|
+#     length_of_words << string.length
+#       sort = 100
+#       length_of_words.each do |length|
+#           if length < sort
+#           sort = length
+#           else
+#             return nil
+#         end
+#     return sort
+#    end
+
+def shortest_string(list_of_words)
+  sort = 100
+  index = 0
+  if list_of_words.count  == 0
+    return nil
+  elsif
+      list_of_words.each_with_index do |string, ind|
+      if string.length < sort
+        sort == string.length
+        index == ind
+      end
+  return list_of_words[index]
+  end
+end
+end
