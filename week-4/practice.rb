369324b8bc87637e4132f348a774ef8d81162dff
euniceschoi <euniceschoi93@gmail.com>
2015-09-21 23:45:29 -0400

    add changes

--- week-4/practice.rb
+++ week-4/practice.rb
@@ -1,3 +1,61 @@
-puts "What do you want?"
-answer=gets.chomp
-puts "WHAT DO YOU MEAN YOU WANT, " + answer.upcase + "! YOU'RE FIRED!"
+# puts "What do you want?"
+# answer=gets.chomp
+# puts "WHAT DO YOU MEAN YOU WANT, " + answer.upcase + "! YOU'RE FIRED!"
+
+# num_start = 5
+
+# num_now = num_start
+
+# while num_now > 2
+#   puts num_now.to_s + ' bottles of beer on the wall, ' + num_now.to_s + 'bottles of beer!'
+#   num_now = num_now - 1
+#   puts 'Take one down, pass it around,' + num_now.to_s + 'bottles of beer on the wall!'
+# end
+
+# puts "2 bottles of beer on the wall, 2 bottles of beer!"
+# puts "Take one down, pass it around, 1 bottle of beer on the wall!"
+# puts "1 bottle of beer on the wall, 1 bottle of beer!"
+# puts "Take one down, pass it around, no more bottles of beer on the wall!"
+
+#say something to grandma argument
+#gets.chomp = what you say
+#generator = random numbers between (20)
+  #year = random number + 1930
+#if what you said is all caps,
+  #grandma replies, "NO not since (year)!"
+#if what you said is NOT in caps,
+  #grandma replies, "HUH?! SPEAK UP, SONNY"
+
+# puts "What do you want to say to grandma?"
+# while true
+#   input = gets.chomp
+#   generator = rand(20)
+#   year = generator + 1930
+
+#   if input == "BYE"
+#     puts 'BYE BYE'
+#     break
+#   end
+
+#   if input.to_s == input.to_s.upcase
+#       puts "NO not since " + year.to_s + " !"
+#   elsif input.to_s == input.to_s.downcase
+#       puts "HUH?! SPEAK UP, SONNY"
+#   end
+# end
+
+# array = []
+# while true
+#   input = gets.chomp
+#   break if input.empty?
+
+#   array << input
+# end
+# puts array.sort
+
+
+I = 1
+II = 2
+III=3
+IV=4
+V = 5
