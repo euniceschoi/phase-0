183c741c4d8e5897c0e4fb41cd3c0be9ac0397f7
euniceschoi <euniceschoi93@gmail.com>
2015-09-30 21:16:42 -0400

    Add reflection

--- week-7/nums_commas.js
+++ week-7/nums_commas.js
@@ -61,17 +61,18 @@ console.log(separateComma(1234000000000))
 // Refactored Solution
 
 function separateComma(num) {
-  var index = 3;
-  var numToString = num.toString().split("").reverse();
-  while (index < numToString.length ){
-    numToString.splice(index, 0, ",")
-    index += 4;
-  }
-  var finalNum = numToString.reverse().join("");
-  return finalNum
+ var numToString = num.toString().split("").reverse();
+ for (var index = 3; index < numToString.length; index += 4){
+   numToString.splice(index, 0, ",")
+ }
+ var finalNum = numToString.reverse().join("");
+ return finalNum
 }
 
-
+console.log(separateComma(12340000000))
+console.log(separateComma(1234))
+console.log(separateComma(12))
+console.log(separateComma(1234000000000))
 
 // Your Own Tests (OPTIONAL)
 
@@ -79,3 +80,18 @@ function separateComma(num) {
 
 
 // Reflection
+
+// What was it like to approach the problem from the perspective of JavaScript? Did you approach the problem differently?
+// It wasn't too difficult approaching the problem. We approached the problem similarly to how we
+// approached Ruby, and my pair and I both had a logical idea of how we wanted to figure this out.
+
+// What did you learn about iterating over arrays in JavaScript?
+// I learned that it is similar to Ruby, but the syntax is a lot different. The for method is very
+// useful in Javascript because it has sort of an outline, and you type in when you want your
+// iterator to end and what to update.
+
+// What was different about solving this problem in JavaScript?
+// Definitely, the syntax
+
+// What built-in methods did you find to incorporate in your refactored solution?
+// We used .length, .split, .reverse, .splice (which was very useful)., and .join!
